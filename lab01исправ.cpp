c5f6d011ff964a565e0afac7e51d55844fe08d4e
ksmushkov-cmd <ksmushkov@bk.ru>
2025-10-01 18:52:36 +0700

    Update lab01исправ.cpp

--- lab01исправ.cpp
+++ lab01исправ.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 int main() {
 
+  double eccentricity, area1, volume1, area2, volume2;
+
   double a;
   cout << " a = ";
   cin >> a;
@@ -19,17 +21,14 @@ int main() {
   cout << " b = ";
   cin >> b;
   
-  //находим величину эксцентриситета 
-  double eccentricity = sqrt(1 - (b * b)/(a * a)); 
+  eccentricity = sqrt(1.0 - (b * b) / (a * a)); 
   
-  //площадь поверхности и объем вытянутого эллипсоида:
-  double area1 = 2.0 * M_PI * (b * b + (a * b * asin(eccentricity)) / eccentricity); 
-  double volume1 = (4.0 * M_PI * a * (b * b)) / 3.0;
+  area1 = 2.0 * M_PI * (b * b + (a * b * asin(eccentricity)) / eccentricity); 
+  volume1 = (4.0 * M_PI * a * (b * b)) / 3.0;
   cout << " area1 = " << area1 << " volume1 = " << volume1 << endl;
   
-  //площадь поверхности и объем сплюснутого эллипсоида:
-  double area2 = 2.0 * M_PI * (a * a + (b * b * log((1.0 + eccentricity)/(1.0 - eccentricity))) / 2.0 * eccentricity);
-  double volume2 = (4.0 * M_PI * (a * a) * b)/3.0;
+  area2 = 2.0 * M_PI * (a * a + (b * b * log((1.0 + eccentricity) / (1.0 - eccentricity))) / 2.0 * eccentricity);
+  volume2 = (4.0 * M_PI * (a * a) * b) / 3.0;
   cout << " area2 = " << area2 << " volume2 = " << volume2 << endl;
 
   return 0; 
